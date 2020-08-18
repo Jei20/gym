ef056d6b02857a1442d28ce6ac565ef278e69da9
Jei20 <tataje_hamett16@hotmail.com>
2020-08-18 12:04:06 -0500

    BotGym
    BotGym

---
+++ QnAGYM/QnAGYM/Models/Answer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QnAGYM.Models
+{
+    public class Answer
+    {
+        public QnAreceivedAnswer [] answers { get; set; }
+    }
+
+    public class QnAreceivedAnswer
+    {
+        public string[] questions { get; set; }
+        public string answer { get; set; }
+        public double score { get; set; }
+        public int id { get; set; }
+        public string source { get; set; }
+        public object[] metadata { get; set; }
+    }
+}
